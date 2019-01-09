ecce7714867bf3f2fa0cae692a2028ff8bacfb60
jim <535258211@qq.com>
2019-01-09 17:18:41 +0800

    feedback 练习

---
+++ 02/feedback/app.js
@@ -0,0 +1,26 @@
+const http = require('http');
+const fs = require('fs');
+
+http
+    .createServer(function (req,res) {
+        console.log('接受到请求');
+        let url = req.url;
+        if (url === '/') {
+            fs.readFile('./views/index.html',function (err,data) {
+                if(err){
+                    return console.log('404 Not Found');
+                }
+                res.end(data);
+            })
+        } else if (url.indexOf('/public/') === 0) {
+            fs.readFile('.'+url,function(err,data){
+                if (err) {
+                    res.end('404 Not Found');
+                }
+                res.end(data);
+            })
+        }
+    })
+    .listen('3000',function () {
+        console.log('server runding...');
+    })
