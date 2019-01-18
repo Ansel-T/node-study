964b2aa40e9e012eb60104940e57a6073f4dcde0
jim <535258211@qq.com>
2019-01-18 16:11:06 +0800

    从json文件中读取数据

--- 04/crud-express/app.js
+++ 04/crud-express/app.js
@@ -8,9 +8,18 @@ app.use('/node_modules/',express.static('./node_modules'));
 app.use('/public/',express.static('./public'));
 
 app.get('/',function (req,res) {
-    res.render('index.html',{
-        courses:['html','css','javascript','nodejs']
-    });
+    fs.readFile('./db.json','utf-8',function (err,data) {
+        if(err){
+            return res.status(500).send('Server error.')
+        }
+        let students = JSON.parse(data).students;
+        console.log(students);
+        res.render('index.html',{
+            courses:['html','css','javascript','nodejs'],
+            students
+        });
+    })
+
 });
 
 app.listen('3000',function () {
