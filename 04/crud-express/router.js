9b29f9bfff2177019253380db624d3f162da1a2d
jim <535258211@qq.com>
2019-02-13 17:14:27 +0800

    添加学员信息

--- 04/crud-express/router.js
+++ 04/crud-express/router.js
@@ -2,7 +2,7 @@ const express = require('express');
 const fs = require('fs');
 const router = express.Router();
 
-router.get('students',function (req,res) {
+router.get('/students',function (req,res) {
     fs.readFile('./db.json','utf-8',function (err,data) {
         if(err){
             return res.status(500).send('Server error.')
@@ -17,18 +17,33 @@ router.get('students',function (req,res) {
 });
 
 router.get('/students/new',function (req,res) {
-
+    res.render('new.html');
 });
 
-router.POST('/students/new',function (req,res) {
-
+router.post('/students/new',function (req,res) {
+    let info = req.body;
+    fs.readFile('./db.json',function(err,data){
+        if(err){
+            return res.end('Not Fount 404');
+        }
+        let studentsObj = JSON.parse(data);
+        info.id = 9;
+        studentsObj.students.push(req.body);
+        studentsObj = JSON.stringify(studentsObj);
+        fs.writeFile('./db.json',studentsObj,function (err) {
+            if(err){
+                return res.end('写入失败');
+            }
+            res.redirect('/students');
+        })
+    })
 });
 
 router.get('/students/edit',function (req,res) {
 
 });
 
-router.POST('/students/edit',function (req,res) {
+router.post('/students/edit',function (req,res) {
 
 });
 
